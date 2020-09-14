0261e7d2b7d8421e0120ffb9ac6d0e93546f1607
Salem Owino <salemowino18@gmail.com>
2020-09-14 18:33:46 +0300

    import error corrected

--- rest/views.py
+++ rest/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, UpdateView, DeleteView,RedirectView,RetrieveAPIView, GenericAPIView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView,RedirectView
+from rest_framework.generics import GenericAPIView,RetrieveAPIView 
 from rest_framework.mixins import ListModelMixin,CreateModelMixin
 from django.http import HttpResponse
 from django.urls import reverse
