b33f425b2314ab477eb8af4585770d6a05dfdab7
Salem Owino <salemowino18@gmail.com>
2020-09-14 10:12:40 +0300

    path

--- mobile/urls.py
+++ mobile/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+# from django.urls import path
+# from django.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    path('', include('rest.urls')),
-    path('', include('django.contrib.auth.urls')),
+    url(r'', include('rest.urls')),
+    url(r'', include('django.contrib.auth.urls')),
 ]
 
